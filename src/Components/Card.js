46f2add4852f246cbb8344406d34e692710477ec
dmitri7751 <silyuk.04@gmail.com>
2024-06-28 14:50:51 +0300

    Feat:Update functionalities

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,7 +6,7 @@ const Card=()=>{
                 <img src="./images/book.png" alt="" />
                 <div className="bottom">
                     <h3 className="title">React JS</h3>
-                    <p className="amount">&#8377;3290</p>
+                    <p className="amount">&#8377;</p>
                 </div>
             </div>
         </>
